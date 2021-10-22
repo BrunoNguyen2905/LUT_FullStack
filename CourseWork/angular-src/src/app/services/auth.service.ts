139194bf50f1ad35076c69a11b25b49d819e383b
BrunoNguyen2905 <thiennhan07.2016@gmail.com>
2021-10-22 19:19:24 +0300

    added angular as frontend and completed user registration

---
+++ CourseWork/angular-src/src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
+// import { tokenNotExpired } from 'angular2-jwt';
+
+@Injectable()
+export class AuthService {
+  authToken: any;
+  user: any;
+  constructor(private http:Http) { }
+
+  registerUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+      .map(res => res.json()) //map method => send back data success true or false with message
+  }
+
+}
